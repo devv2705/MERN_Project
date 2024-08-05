46002b29e295ece29499818a8a846c8c13a641e7
p.devv <pd27052003@gmail.com>
2024-08-05 23:53:58 +0530

    connect online DB on atlas

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
-
 const app=express()
+//dotenv is used for hide sensitive information
+require('dotenv').config()
 const router=require("../server/routes/auth-router") 
 const connect_dB=require("../server/utils/db")
 
@@ -12,7 +13,7 @@ app.use("/api/auth",router);
 const PORT=4000;
 
 connect_dB().then(()=>{
-    
+
 app.listen(PORT,()=>{
     console.log(`server is running at port : ${PORT}`)
 })
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose")
 
-const URI="mongodb://localhost/mern_db"
+
+const URI=process.env.MONGODB_URI
  const connect_dB=async ()=>{
     try{
 
