f9cf5b40b4e8522365a598843da871dcbcb2184e
p.devv <pd27052003@gmail.com>
2024-08-06 22:23:03 +0530

    zod validation

--- server/routes/auth-router.js
+++ server/routes/auth-router.js
@@ -2,11 +2,13 @@ const express=require('express');
 
 const router=express.Router();
 const authControllers=require("../controllers/auth-controller")
+const validate=require("../middelwares/validate-middleware")
+const signupSchema=require("../validator/auth-validator")
 
 
 router.route("/").get(authControllers.home)
 
-router.route("/registration").post(authControllers.register)
+router.route("/registration").post(validate(signupSchema),authControllers.register)
 router.route("/login").post(authControllers.login)
 
 module.exports =router 
---
+++ server/validator/auth-validator.js
@@ -0,0 +1,23 @@
+const zod=require("zod")
+
+//creating object schema
+
+const signupSchema=zod.object({
+    username:zod.string({required_error:"name is require"})
+    .trim().min(3,{message:"name must be of 3 char"})
+    .max(255,{message:"Name must be not more than 255 char"}),
+
+    email:zod.string({required_error:"email is require"})
+    .trim().email({message:"Invalid Email !!"}).min(5,{message:"email must be of 3 char"})
+    .max(255,{message:"email must be not more than 255 char"}),
+
+    phone:zod.string({required_error:"phonenumber is require"})
+    .trim().min(10,{message:"number must be of 10 char"})
+    .max(20,{message:"number must be not more than 20 char"}),
+
+    password:zod.string({required_error:"password is require"})
+    .trim().min(7,{message:"name must be of 7 char"})
+    .max(1024,{message:"Name must be not more than 1024 char"}),
+})
+
+module.exports=signupSchema;
