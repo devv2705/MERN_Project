cc99461d7c936d0b5f22ae2a781701fb62202d87
p.devv <pd27052003@gmail.com>
2024-08-08 00:16:18 +0530

    registration and login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,3 +1,72 @@
+import { useState } from "react";
+
 export const Login=()=>{
-    return <h1>Hello Login Page</h1>
+
+    const [user,setUser]=useState({
+        email:"",
+        password:"",
+
+    })
+
+    const handleInput=(e)=>{
+        let name=e.target.name
+        let value=e.target.value
+
+        setUser({
+            ...user,
+            [name]:value,
+        })
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        alert(user)
+    }
+    return <>
+    <section>
+       <main>
+           <div className="section-registration">
+               <div className="container grid grid-two-cols">
+                   <div className="registration-image">
+                       <img src="/image/regi.jpg" alt="registation image" width="500" height="500" />
+                   </div>
+                   <div className="registration-form">
+                       <h1 className="main-heading">Registration Form</h1>
+                       <br />
+                       <form onSubmit={handleSubmit}>
+                           <div>
+                               <label htmlFor="email">email</label>
+                               <input 
+                               type="email" 
+                               name="email" 
+                               placeholder="Enter Email" 
+                               id="email" 
+                               required
+                               autoComplete="off" 
+                               value={user.email}
+                               onChange={handleInput}
+                               />
+
+                           </div>
+                           <div>
+                               <label htmlFor="password">password</label>
+                               <input type="password" 
+                               name="password" 
+                               placeholder="Enter password" 
+                               id="password" 
+                               required 
+                               autoComplete="off"
+                               value={user.password}
+                               onChange={handleInput}
+                               />
+
+                           </div>
+                           <br />
+                           <button type="submit" className="btn btn-submit">Login</button>
+                       </form>
+                   </div>
+               </div>
+           </div>
+       </main>
+    </section>
+   </>
 };
--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -1,3 +1,91 @@
+import { useState } from "react";
+
 export const Registration=()=>{
-    return <h1>Hello Registration Page</h1>
+    const [user,setUser]=useState({
+        username:"",
+        email:"",
+        phone:"",
+        password:""
+
+    });
+
+    //handling the input value
+    const handleInput=(e) =>{
+        let name=e.target.name;
+        let value=e.target.value;
+
+        setUser({
+            ...user,
+            [name]:value,
+        })
+    }
+   const handleSubmit=(e)=> {
+
+    e.preventDefault();
+    alert(user)
+
+   }
+    return <>
+     <section>
+        <main>
+            <div className="section-registration">
+                <div className="container grid grid-two-cols">
+                    <div className="registration-image">
+                        <img src="/image/regi.jpg" alt="registation image" width="500" height="500" />
+                    </div>
+                    <div className="registration-form">
+                        <h1 className="main-heading">Registration Form</h1>
+                        <br />
+                        <form onSubmit={handleSubmit}>
+                            <div>
+                                <label htmlFor="username">username</label>
+                                <input type="text" 
+                                name="username" 
+                                placeholder="Enter Username" 
+                                id="username" 
+                                required 
+                                autoComplete="off"
+                                value={user.username}
+                                onChange={handleInput}
+                                />
+
+                            </div>
+                            <div>
+                                <label htmlFor="email">email</label>
+                                <input 
+                                type="email" 
+                                name="email" 
+                                placeholder="Enter Email" 
+                                id="email" 
+                                required
+                                autoComplete="off" 
+                                value={user.email}
+                                onChange={handleInput}
+                                />
+
+                            </div>
+                            <div>
+                                <label htmlFor="phone">phone</label>
+                                <input type="number" name="phone" placeholder="Enter Phonenumber" id="phone" required autoComplete="off" 
+                                value={user.phone}
+                                onChange={handleInput}/>
+
+                            </div>
+                            <div>
+                                <label htmlFor="password">password</label>
+                                <input type="password" name="password" placeholder="Enter password" id="password" required autoComplete="off"
+                                value={user.password}
+                                onChange={handleInput}
+                                />
+
+                            </div>
+                            <br />
+                            <button type="submit" className="btn btn-submit">Register</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </main>
+     </section>
+    </>
 };
