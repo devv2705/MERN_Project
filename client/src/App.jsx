d8361de78bd51e77ab401c12e117f8c5b5e74f1b
p.devv <pd27052003@gmail.com>
2024-08-07 16:18:05 +0530

    created navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import {Contact} from "./pages/Contact"
 import {Service} from "./pages/Service"
 import {Registration} from "./pages/Registration"
 import {Login} from "./pages/Login"
+import { Navbar } from "./components/Navbar";
 
 export const App=()=>{
   return (
   <>
     <BrowserRouter>
+    <Navbar/>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+import "./Navbar.css"
+export const Navbar=()=>{
+    return (<>
+    <header>
+        <div className="container">
+            <div className="logo-brand">
+                <a href="/">DevPrajapati</a>
+
+            </div>
+            <nav>
+                <ul>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
+                    <li><NavLink to="/service">Service</NavLink></li>
+                    <li><NavLink to="/login">Login</NavLink></li>
+                    <li><NavLink to="/registration">Registration</NavLink></li>
+                    
+                    
+                </ul>
+            </nav>
+        </div>
+    </header>
+    </>
+    );
+}
